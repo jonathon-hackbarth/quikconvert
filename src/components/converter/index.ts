9ac2dfb350b69c613a064006e4f2ac3b9ac04875
jonathon-hackbarth <jonathon.hackbarth@gmail.com>
2025-12-08 21:50:07 -0600

    Remove old converter components and add testing dependencies

--- src/components/converter/index.ts
+++
@@ -1,7 +0,0 @@
-export { BaseConverter } from "./base-converter";
-export { ConversionDisplay } from "./conversion-display";
-export { QuantityInput } from "./quantity-input";
-export { UnitSelector } from "./unit-selector";
-export { ConverterSection } from "./converter-section";
-export { Header } from "./header";
-export { Footer } from "./footer";
